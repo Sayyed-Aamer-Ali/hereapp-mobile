7d23da9d83c198ce5b0dfe8ab4d0343abb6c834a
Sayyed-Aamer-Ali <sayyedaamer.asharali@infovision.com>
2025-09-04 11:24:33 +0530

    Baseurl changes

--- src/services/BaseUrl.js
+++ src/services/BaseUrl.js
@@ -1,9 +1,9 @@
 import {BASE_URL_FROM_ENV, WEB_SOCKET_URL_FROM_ENV} from '@env';
 
-console.log(BASE_URL_FROM_ENV)
+console.log(BASE_URL_FROM_ENV);
 // const BaseUrl = 'http://10.0.2.2:4000/';
-const BaseUrl = 'http://18.207.156.70:4000/';
-// const BaseUrl = 'https://api.hereapp.io/'
+// const BaseUrl = 'http://18.207.156.70:4000/';
+const BaseUrl = 'https://api.hereapp.io/';
 
 // export const SocketUrl = 'http://167.114.144.249:5056';
 
